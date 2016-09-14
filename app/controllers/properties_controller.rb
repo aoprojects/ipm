a712a3a363748b57d8ad75f65ae0c256eb8532ee
Alison Ostendorf <aostendorf@gmail.com>
2016-09-14 18:01:03 -0400

    add skeleton of properties controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,35 @@
 class PropertiesController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+	def index
+		@properties = Property.all
+	end
+
+	def new
+		# @property = current_user.property.build(property_params)
+
+	end
+
+	def create 
+
+	end
+
+	def edit
+
+	end
+
+	def update 
+
+	end 
+
+	def destroy
+
+	end
+
+private
+	def property_params
+
+	end
+
+
 end
